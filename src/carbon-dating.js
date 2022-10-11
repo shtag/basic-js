a4ffa1c0a9c7a04c7c92df51f1090d33efe37f30
shtag <vasyakanishchev@gmail.com>
2022-10-11 20:09:40 +0300

    feat complete carbon dating, count cats

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function dateSample(sampleActivity) {
+  console.log(sampleActivity)
+  if(typeof sampleActivity === 'string' && +sampleActivity < MODERN_ACTIVITY && +sampleActivity > 0){
+    let k = 0.693 / HALF_LIFE_PERIOD;
+    let t = (Math.log(MODERN_ACTIVITY / +sampleActivity)) / k;
+    return Math.ceil(t);
+
+  } else return false
 }
 
 module.exports = {
--- src/count-cats.js
+++ src/count-cats.js
@@ -14,9 +14,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats(matrix) {
+  let counter = 0;
+  matrix.forEach(element => {
+    element.forEach(element => {
+      if(element == '^^') counter++
+    })
+  })
+  return counter;
 }
 
 module.exports = {
